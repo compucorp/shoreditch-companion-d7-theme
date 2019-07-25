1db79132934d5a76f566dfe0b1afca8b7351c27f
Alessandro Verdura <alessandro@compucorp.co.uk>
2019-07-25 10:52:58 +0200

    SHOR-144: Add sass and sass:watch gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { dest, series, src, watch } = require('gulp');
+
+function sass () {
+  const sass = require('gulp-sass');
+  sass.compiler = require('node-sass');
+
+  return src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
+}
+
+function sassWatch () {
+  watch('./scss/**/*.scss', sass);
+}
+
+Object.assign(exports, {
+  'default': sass,
+  'sass': sass,
+  'sass:watch': sassWatch
+});
